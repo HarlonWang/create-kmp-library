b58cdf3468ba4c4531c88b6a09050d4b25a7acb4
wanghl <wanghl@pingpongx.com>
2026-01-14 17:35:41 +0800

    init

--- utils/getCommand.ts
+++
@@ -1,7 +0,0 @@
-export default function getCommand(packageManager: string, scriptName: string): string {
-    if (scriptName === 'install') {
-        return packageManager === 'yarn' ? 'yarn' : `${packageManager} install`
-    }
-
-    return packageManager === 'npm' ? `npm run ${scriptName}` : `${packageManager} ${scriptName}`
-}
