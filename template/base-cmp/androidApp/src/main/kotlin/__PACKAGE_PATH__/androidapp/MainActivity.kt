b44b0e6bbe1f2c84c4b7d834d68b7a3e7c8f30d1
wanghl <wanghl@pingpongx.com>
2026-01-15 18:48:55 +0800

    init

--- template/base-cmp/androidApp/src/main/kotlin/__PACKAGE_PATH__/androidapp/MainActivity.kt
+++ template/base-cmp/androidApp/src/main/kotlin/__PACKAGE_PATH__/androidapp/MainActivity.kt
@@ -3,11 +3,14 @@ package __PACKAGE__.androidapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import __PACKAGE__.LibraryRoot
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.getInsetsController(window, window.decorView).isAppearanceLightStatusBars = true
 
         setContent {
             LibraryRoot()
--- template/base-cmp/library/src/commonMain/kotlin/__PACKAGE_PATH__/LibraryRoot.kt
+++ template/base-cmp/library/src/commonMain/kotlin/__PACKAGE_PATH__/LibraryRoot.kt
@@ -1,9 +1,21 @@
 package __PACKAGE__
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun LibraryRoot() {
-    Text(Greeting().greet())
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .windowInsetsPadding(WindowInsets.safeDrawing)
+    ) {
+        Text(Greeting().greet())
+    }
 }
